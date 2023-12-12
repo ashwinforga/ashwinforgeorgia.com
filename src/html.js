aa83191e77a07cd42b532720c54765842e33b2f7
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-12-12 14:20:59 +0000

    add preview

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
+        <meta property="og:image" content="https://ashwinforgeorgia.com/images/preview.jpeg" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
