9bb7775b4db09db17c825e061a795852b6de8155
Ashwin Ramaswami <aramaswamis@gmail.com>
2024-01-08 20:54:15 +0000

    Add privacy policy

---
+++ src/pages/privacy.js
@@ -0,0 +1,3 @@
+export default () => (
+    <iframe width="100%" height="1000" src="https://docs.google.com/document/d/e/2PACX-1vSUpkpixRCnCcCOaPvjybk0LgH6S2bK-xiJRd3iIdggE6hq2lOqVoFi9GNi2czR9QMHBn2diNFlHjS-/pub?embedded=true"></iframe>
+);
